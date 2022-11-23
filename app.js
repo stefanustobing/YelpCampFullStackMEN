4c10d71568e9a09ca8ae4cd39058e01ee19e5679
Stefanus Lumban Tobing <stefanustobing@gmail.com>
2022-11-23 16:35:23 +0100

    First commit Very basic CRUD functionalities

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+const methodOverride = require('method-override');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp2', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log('Database Connected!');
+    })
+    .catch((err) => {
+        console.log('Oh NO, Mongo Connection Error!');
+        console.log(err);
+    });
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndRemove(id);
+    res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit', { campground });
+});
+
+
+app.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
